758f228aea95ade26734d183836b5e3a01549364
Nolant108 <officialstackemup@gmail.com>
2022-02-28 21:33:29 -0800

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const { MessageEmbed, commands } = require('discord.js');
+const config = require("./config.json");
+
+const client = new Discord.Client({
+
+    intents: [
+
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS",
+        "GUILD_EMOJIS_AND_STICKERS",
+        "DIRECT_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS"
+
+    ]
+
+})
+
+client.on('ready', () => {
+
+    console.log(`Logged in as ${client.user.tag}`)
+
+    client.user.setStatus('online')
+    client.user.setActivity( {
+        name: '!help | Im like a stuffed animal... Whom came to life!'
+    
+    });
+})
+
+client.login(config.token);

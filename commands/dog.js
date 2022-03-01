50fc55620357044ba0831263020d1a6cc5e940aa
Nolant108 <officialstackemup@gmail.com>
2022-03-01 13:10:28 -0800

    update

---
+++ commands/dog.js
@@ -0,0 +1,20 @@
+const { default: axios } = require('axios');
+const { Discord, message, member } = require('discord.js');
+const JokeAPI = require('sv443-joke-api');
+var fs = require('fs');
+
+module.exports = {
+    name: 'dog',
+    description: 'Random dog img display',
+    execute(message, args, async) {
+
+
+        axios
+        .get('https://api.thedogapi.com/v1/images/search')
+        .then((res) => {
+
+            message.reply(res.data[0].url)
+
+        })
+ }
+};
